a3c1f7a2b3146bda8055dc2306b5fb1f59164b00
janepark1007 <longhetaihe2015@gmail.com>
2026-08-13 00:22:53 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-window.MIRIM_SUPABASE = {
+window.MIRIM_CONFIG = {
   url: "https://lxxefxaehqbvqyarldsq.supabase.co",
-  publishableKey: "sb_publishable_dax31BfVkfMrm7wIr6QqBQ_6CCe0Gbd"
+  publishableKey: "sb_publishable_dax31BfVkfMrm7wIr6QqBQ_6CCe0Gbd",
+  adminPin: "2589"
 };
